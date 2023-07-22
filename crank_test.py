0b1ae9fdceb5b31a9e7f9f91a9de676d545f3ab2
Wissg <wissal.ghoudi@gmail.com>
2023-07-22 17:36:42 +0200

    ++

--- crank_test.py
+++ crank_test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def Kronecker(j, i):
     if j == i:
         return 1
@@ -39,8 +40,11 @@ def Crank_Nicolson(S0, r, T, N, M, x_max, x_min, sigma):
             A[i] = - deltat * 0.25 * (-(i * r + 1/(T * delta_x)) - (sigma**2) * (i**2))
             B[i] = deltat * 0.25 * (-(i * r + 1/(T * delta_x)) + (sigma**2) * (i**2))
             D[i] = 1 + deltat * 0.5 * (sigma**2) * (i**2)
-            K[n, i] = A[i] * V[n - 1, i + 1] + B[i] * V[n - 1, i - 1] + D[i] * V[n - 1, i] - A[i] * V[n, i + 1] - Kronecker(1, i) * B[1] * S0
-            # K[n, i] = A[i] * V[n, i - 1] + B[i] * V[n, i + 1] + D[i] * V[n, i] - A[i] * V[n, i + 1] - Kronecker(1, i) * B[1] * S0
+            h = 1 / (r * T) * (1 - np.exp(-r * (T - (n - 1) * deltat)))
+            # K[n, i] = A[i] * V[n - 1, i + 1] + B[i] * V[n - 1, i - 1] + D[i] * V[n - 1, i] - Kronecker(1, i) * B[1] * h
+            # K[n, i] = A[i] * V[n, i - 1] + B[i] * V[n, i + 1] + D[i] * V[n, i] - Kronecker(1, i) * B[1] * h
+            K[n, i] = B[i] * V[n, i - 1] + A[i] * V[n, i + 1] + D[i] * V[n, i] - Kronecker(1, i) * B[1] * h
+
 
         D2[1] = D[1]
         K2[n, 1] = K[n, 1]
@@ -52,12 +56,26 @@ def Crank_Nicolson(S0, r, T, N, M, x_max, x_min, sigma):
             V[n + 1, i] = (K2[n, i] - A[i] * V[n + 1, i + 1]) / D2[i]
 
     print(V)
-    plt.plot(x, V[0, :], label="t = 0")
-    plt.plot(x, V[M // 2, :], label="t = T/2")
-    plt.xlabel('Variable x')
-    plt.ylabel('Fonction f')
-    plt.title('Fonction f en 2 dimensions en t=0 et t=T/2')
-    plt.legend()
+    # plt.plot(x, V[0, :], label="t = 0")
+    # plt.plot(x, V[M // 2, :], label="t = T/2")
+    # plt.xlabel('Variable x')
+    # plt.ylabel('Fonction f')
+    # plt.title('Fonction f en 2 dimensions en t=0 et t=T/2')
+    # plt.legend()
+    # plt.show()
+
+    # Create a meshgrid to plot the surface
+    X, T = np.meshgrid(x, t)
+
+    # Plot the surface at t = 0
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.plot_surface(X, T, V, cmap='viridis')
+    ax.set_xlabel('Variable x')
+    ax.set_ylabel('Time t')
+    ax.set_zlabel('Function f')
+    ax.set_title('2D Surface Plot at t = 0')
+    plt.savefig("Graph/crank.png")
     plt.show()
     return V
 
@@ -73,6 +91,14 @@ sigma = 0.3
 N = 99
 M = 999
 
+S = np.linspace(1,50,50)
+V = np.zeros(len(S))
+x = np.linspace(Xmin, Xmax, N + 2)
+
 # Appel de la fonction Crank_Nicolson
-V = Crank_Nicolson(S0, r, T, N, M, Xmax, Xmin, sigma)
+
+Crank_Nicolson(S0, r, T, N, M, Xmax, Xmin, sigma)
+
+fig = plt.figure(figsize =(14, 9))
+ax = plt.axes(projection ='3d')
 
