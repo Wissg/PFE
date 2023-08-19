cad511957c4a802f058ef3dbb6348f9c06db605e
Wissg <wissal.ghoudi@gmail.com>
2023-08-19 16:45:54 +0200

    ++

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from scipy.stats import norm
 import math
 import statistics
 from scipy import integrate as intg
+from mpl_toolkits import mplot3d
 
 
 def asian_option_price_call(S0, r, K, T, N, sigma, Nmc):
@@ -23,13 +24,32 @@ def asian_option_price_call(S0, r, K, T, N, sigma, Nmc):
             S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
         S_avg[i] = np.mean(S)
         S_avg_geo[i] = statistics.geometric_mean(S)
-        payoffs[i] = np.maximum(S_avg[i] - K, 0)
-        payoffs_geo[i] = np.maximum(S_avg_geo[i] - K, 0)
+        payoffs[i] = np.maximum(S_avg[i]/T - K, 0)
+        payoffs_geo[i] = np.maximum(S_avg_geo[i]/T - K, 0)
 
     option_price = np.exp(-r * T) * np.mean(payoffs)
     option_price_geo = np.exp(-r * T) * np.mean(payoffs_geo)
 
     return option_price, option_price_geo
+
+S0 = np.linspace(10,200,100)
+K = 20
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+
+price_arith = np.zeros(len(S0))
+price_geo = np.zeros(len(S0))
+
+for i in range(len(S0)):
+    price_arith[i], price_geo[i] = asian_option_price_call(S0[i], r, K, T, N, sigma, Nmc)
+
+plt.plot(S0, price_arith, label="Prix Call arithmétique")
+plt.plot(S0, price_geo, label="Prix Call géométrique")
+plt.legend()
+plt.show()
 
 
 def calculate_floating_strike(S):
@@ -54,13 +74,32 @@ def asian_option_price_call_flottant(S0, r, K, T, N, sigma, Nmc):
             S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
         S_avg[i] = np.mean(S)
         S_avg_geo[i] = statistics.geometric_mean(S)
-        payoffs[i] = np.maximum(S[T] - S_avg[i], 0)  # Utilisation du dernier prix d'exercice flottant
-        payoffs_geo[i] = np.maximum(S[T] - S_avg_geo[i], 0)  # Utilisation du dernier prix d'exercice flottant
+        payoffs[i] = np.maximum(S[N] - S_avg[i]/T, 0)  # Utilisation du dernier prix d'exercice flottant
+        payoffs_geo[i] = np.maximum(S[N] - S_avg_geo[i]/T, 0)  # Utilisation du dernier prix d'exercice flottant
 
     option_price = np.exp(-r * T) * np.mean(payoffs)
     option_price_geo = np.exp(-r * T) * np.mean(payoffs_geo)
 
     return option_price, option_price_geo
+
+S0 = np.linspace(10,200,100)
+K = 20
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+
+price_arith = np.zeros(len(S0))
+price_geo = np.zeros(len(S0))
+
+for i in range(len(S0)):
+    price_arith[i], price_geo[i] = asian_option_price_call_flottant(S0[i], r, K, T, N, sigma, Nmc)
+
+plt.plot(S0, price_arith, label="Prix Call arithmétique")
+plt.plot(S0, price_geo, label="Prix Call géométrique")
+plt.legend()
+plt.show()
 
 
 def asian_option_price_put_flottant(S0, r, T, N, sigma, Nmc):
@@ -80,13 +119,32 @@ def asian_option_price_put_flottant(S0, r, T, N, sigma, Nmc):
         K = S[N]  # Mise à jour du prix d'exercice flottant
         S_avg[i] = np.mean(S)
         S_avg_geo[i] = statistics.geometric_mean(S)
-        payoffs[i] = np.maximum(K - S_avg[i], 0)  # Utilisation du dernier prix d'exercice flottant
-        payoffs_geo[i] = np.maximum(K - S_avg_geo[i], 0)  # Utilisation du dernier prix d'exercice flottant
+        payoffs[i] = np.maximum(S_avg[i]/T - S[N], 0)  # Utilisation du dernier prix d'exercice flottant
+        payoffs_geo[i] = np.maximum(S_avg_geo[i]/T - S[N], 0)  # Utilisation du dernier prix d'exercice flottant
 
     option_price = np.exp(-r * T) * np.mean(payoffs)
     option_price_geo = np.exp(-r * T) * np.mean(payoffs_geo)
 
     return option_price, option_price_geo
+
+S0 = np.linspace(10,200,100)
+K = 20
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+
+price_arith = np.zeros(len(S0))
+price_geo = np.zeros(len(S0))
+
+for i in range(len(S0)):
+    price_arith[i], price_geo[i] = asian_option_price_put_flottant(S0[i], r, T, N, sigma, Nmc)
+
+plt.plot(S0, price_arith, label="Prix Put arithmétique")
+plt.plot(S0, price_geo, label="Prix Put géométrique")
+plt.legend()
+plt.show()
 
 
 def asian_option_price_put(S0, r, K, T, N, sigma, Nmc):
@@ -104,14 +162,280 @@ def asian_option_price_put(S0, r, K, T, N, sigma, Nmc):
             S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
         S_avg[i] = np.mean(S)
         S_avg_geo[i] = statistics.geometric_mean(S)
-        payoffs[i] = np.maximum(K - S_avg[i], 0)
-        payoffs_geo[i] = np.maximum(K - S_avg_geo[i], 0)
+        payoffs[i] = np.maximum(K - S_avg[i]/T, 0)
+        payoffs_geo[i] = np.maximum(K - S_avg_geo[i]/T, 0)
 
     option_price = np.exp(-r * T) * np.mean(payoffs)
     option_price_geo = np.exp(-r * T) * np.mean(payoffs_geo)
 
     return option_price, option_price_geo
 
+S0 = np.linspace(10,200,100)
+K = 100
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+
+price_arith = np.zeros(len(S0))
+price_geo = np.zeros(len(S0))
+
+for i in range(len(S0)):
+    price_arith[i], price_geo[i] = asian_option_price_put(S0[i], r, K, T, N, sigma, Nmc)
+
+plt.plot(S0, price_arith, label="Prix Put arithmétique")
+plt.plot(S0, price_geo, label="Prix Put géométrique")
+plt.legend()
+plt.show()
+
+def asian_option_price_call_t(St, At, t, r, K, T, N, sigma, Nmc):
+    dt = (T - t) / N
+    S_avg = np.zeros(Nmc)
+    S_avg_geo = np.zeros(Nmc)
+    S = np.zeros(N + 1)
+    payoffs = np.zeros(Nmc)
+    payoffs_geo = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        S[0] = St
+        for j in range(1, N + 1):
+            dW = np.sqrt(dt) * np.random.randn(1)
+            S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
+        S_avg[i] = np.mean(S)
+        S_avg_geo[i] = statistics.geometric_mean(S)
+        payoffs[i] = np.maximum((At*t + S_avg[i])/T - K, 0)
+        payoffs_geo[i] = np.maximum((At*t + S_avg_geo[i])/T - K, 0)
+
+    option_price = np.exp(-r * (T - t)) * np.mean(payoffs)
+    option_price_geo = np.exp(-r * (T - t)) * np.mean(payoffs_geo)
+
+    return option_price, option_price_geo
+
+St = np.linspace(10,200,50)
+K = 20
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+At = 100
+t = T/2
+
+price_arith = np.zeros(len(St))
+price_geo = np.zeros(len(St))
+
+for i in range(len(St)):
+    price_arith[i], price_geo[i] = asian_option_price_call_t(St[i], At, t, r, K, T, N, sigma, Nmc)
+
+plt.plot(St, price_arith, label="Prix Call arith")
+plt.plot(St, price_geo, label="Prix Call geo")
+plt.legend()
+plt.show()
+
+St = np.linspace(10,200,50)
+At = np.linspace(10,200,50)
+price_arith = np.zeros(shape=(len(St),len(At)))
+price_geo = np.zeros(shape=(len(St),len(At)))
+
+for i in range(len(St)):
+    for j in range(len(At)):
+        price_arith[i, j], price_geo[i, j] = asian_option_price_call_t(St[i], At[j], t, r, K, T, N, sigma, Nmc)
+
+A, S = np.meshgrid(At, St)
+ax = plt.axes(projection= '3d')
+ax.plot_surface(S, A, price_arith, cmap='viridis', edgecolor='none')
+ax.view_init(elev=10, azim=1)
+ax.set_xlabel('Variable St')
+ax.set_ylabel('Variable At')
+ax.set_zlabel('Price')
+plt.show()
+
+def asian_option_price_put_t(St, At, t, r, K, T, N, sigma, Nmc):
+    dt = (T - t) / N
+    S_avg = np.zeros(Nmc)
+    S_avg_geo = np.zeros(Nmc)
+    S = np.zeros(N + 1)
+    payoffs = np.zeros(Nmc)
+    payoffs_geo = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        S[0] = St
+        for j in range(1, N + 1):
+            dW = np.sqrt(dt) * np.random.randn(1)
+            S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
+        S_avg[i] = np.mean(S)
+        S_avg_geo[i] = statistics.geometric_mean(S)
+        payoffs[i] = np.maximum(K - (At*t + S_avg[i])/T, 0)
+        payoffs_geo[i] = np.maximum(K - (At*t + S_avg_geo[i])/T, 0)
+
+    option_price = np.exp(-r * (T - t)) * np.mean(payoffs)
+    option_price_geo = np.exp(-r * (T - t)) * np.mean(payoffs_geo)
+
+    return option_price, option_price_geo
+
+St = np.linspace(10,200,50)
+K = 200
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+At = 100
+t = T/2
+
+price_arith = np.zeros(len(St))
+price_geo = np.zeros(len(St))
+
+for i in range(len(St)):
+    price_arith[i], price_geo[i] = asian_option_price_put_t(St[i], At, t, r, K, T, N, sigma, Nmc)
+
+plt.plot(St, price_arith, label="Prix Put arith")
+plt.plot(St, price_geo, label="Prix Put geo")
+plt.legend()
+plt.show()
+
+St = np.linspace(10,200,50)
+At = np.linspace(10,200,50)
+price_arith = np.zeros(shape=(len(St),len(At)))
+price_geo = np.zeros(shape=(len(St),len(At)))
+
+for i in range(len(St)):
+    for j in range(len(At)):
+        price_arith[i, j], price_geo[i, j] = asian_option_price_put_t(St[i], At[j], t, r, K, T, N, sigma, Nmc)
+
+A, S = np.meshgrid(At, St)
+ax = plt.axes(projection= '3d')
+ax.plot_surface(S, A, price_arith, cmap='viridis', edgecolor='none')
+ax.view_init(elev=10, azim=1)
+ax.set_xlabel('Variable St')
+ax.set_ylabel('Variable At')
+ax.set_zlabel('Price')
+plt.show()
+
+def asian_option_price_call_flottant_t(St, At, t, r, K, T, N, sigma, Nmc):
+    dt = (T - t) / N
+    S_avg = np.zeros(Nmc)
+    S_avg_geo = np.zeros(Nmc)
+    S = np.zeros(N + 1)
+    payoffs = np.zeros(Nmc)
+    payoffs_geo = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        S[0] = St
+        for j in range(1, N + 1):
+            dW = np.sqrt(dt) * np.random.randn(1)
+            S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
+        S_avg[i] = np.mean(S)
+        S_avg_geo[i] = statistics.geometric_mean(S)
+        payoffs[i] = np.maximum(S[N] - (At*t + S_avg[i])/T, 0)
+        payoffs_geo[i] = np.maximum(S[N] - (At*t + S_avg_geo[i])/T, 0)
+
+    option_price = np.exp(-r * (T - t)) * np.mean(payoffs)
+    option_price_geo = np.exp(-r * (T - t)) * np.mean(payoffs_geo)
+
+    return option_price, option_price_geo
+
+St = np.linspace(10,200,50)
+K = 100
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+At = 100
+t = T/2
+
+price_arith = np.zeros(len(St))
+price_geo = np.zeros(len(St))
+
+for i in range(len(St)):
+    price_arith[i], price_geo[i] = asian_option_price_call_flottant_t(St[i], At, t, r, K, T, N, sigma, Nmc)
+
+plt.plot(St, price_arith, label="Prix Call arith")
+plt.plot(St, price_geo, label="Prix Call geo")
+plt.legend()
+plt.show()
+
+St = np.linspace(10,200,50)
+At = np.linspace(10,200,50)
+price_arith = np.zeros(shape=(len(St),len(At)))
+price_geo = np.zeros(shape=(len(St),len(At)))
+
+for i in range(len(St)):
+    for j in range(len(At)):
+        price_arith[i, j], price_geo[i, j] = asian_option_price_call_flottant_t(St[i], At[j], t, r, K, T, N, sigma, Nmc)
+
+A, S = np.meshgrid(At, St)
+ax = plt.axes(projection= '3d')
+ax.plot_surface(S, A, price_arith, cmap='viridis', edgecolor='none')
+ax.view_init(elev=10, azim=1)
+ax.set_xlabel('Variable St')
+ax.set_ylabel('Variable At')
+ax.set_zlabel('Price')
+plt.show()
+
+def asian_option_price_put_flottant_t(St, At, t, r, K, T, N, sigma, Nmc):
+    dt = (T - t) / N
+    S_avg = np.zeros(Nmc)
+    S_avg_geo = np.zeros(Nmc)
+    S = np.zeros(N + 1)
+    payoffs = np.zeros(Nmc)
+    payoffs_geo = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        S[0] = St
+        for j in range(1, N + 1):
+            dW = np.sqrt(dt) * np.random.randn(1)
+            S[j] = S[j - 1] * np.exp((r - 0.5 * sigma ** 2) * dt + sigma * dW)
+        S_avg[i] = np.mean(S)
+        S_avg_geo[i] = statistics.geometric_mean(S)
+        payoffs[i] = np.maximum((At*t + S_avg[i])/T - S[N], 0)
+        payoffs_geo[i] = np.maximum((At*t + S_avg_geo[i])/T - S[N], 0)
+
+    option_price = np.exp(-r * (T - t)) * np.mean(payoffs)
+    option_price_geo = np.exp(-r * (T - t)) * np.mean(payoffs_geo)
+
+    return option_price, option_price_geo
+
+St = np.linspace(10,200,50)
+K = 20
+r = 0.05
+sigma = 0.5
+T = 1
+N = 100
+Nmc = 1000
+At = 100
+t = T/2
+
+price_arith = np.zeros(len(St))
+price_geo = np.zeros(len(St))
+
+for i in range(len(St)):
+    price_arith[i], price_geo[i] = asian_option_price_put_flottant_t(St[i], At, t, r, K, T, N, sigma, Nmc)
+
+plt.plot(St, price_arith, label="Prix Put arith")
+plt.plot(St, price_geo, label="Prix Put geo")
+plt.legend()
+plt.show()
+
+St = np.linspace(10,200,50)
+At = np.linspace(10,200,50)
+price_arith = np.zeros(shape=(len(St),len(At)))
+price_geo = np.zeros(shape=(len(St),len(At)))
+
+for i in range(len(St)):
+    for j in range(len(At)):
+        price_arith[i, j], price_geo[i, j] = asian_option_price_put_flottant_t(St[i], At[j], t, r, K, T, N, sigma, Nmc)
+
+A, S = np.meshgrid(At, St)
+ax = plt.axes(projection= '3d')
+ax.plot_surface(S, A, price_arith, cmap='viridis', edgecolor='none')
+ax.view_init(elev=10, azim=1)
+ax.set_xlabel('Variable St')
+ax.set_ylabel('Variable At')
+ax.set_zlabel('Price')
+plt.show()
 
 def delta(S0, r, K, T, sigma, N, Nmc, h):
     S = np.zeros(N + 1)
@@ -159,7 +483,8 @@ def gamma(S0, r, K, T, sigma, N, Nmc, h):
     C_up = np.exp(-r * T) * np.mean(payoff_up)
     C_down = np.exp(-r * T) * np.mean(payoff_down)
 
-    gamma = ((C_up - C) / (S0 * h) - (C - C_down) / (S0 * h)) / (0.5 * S0 * h)
+    gamma = ((C_up - C) / (S0 * h) - (C - C_down) / (S0 * h)) / (2 * S0 * h)
+    #gamma = (C_up - 2*C + C_down)/h**2
     return gamma
 
 
@@ -313,6 +638,63 @@ def Crank_Nicolson(r, T, N, M, x_max, x_min, sigma):
     return V
 
 
+def Crank_Nicolson2(r, T, N, M, x_max, x_min, sigma):
+    x = np.linspace(x_min, x_max, N + 1)
+    t = np.linspace(0, T, M + 1)
+    deltat = T / M
+    delta_x = (x_max - x_min) / N
+    
+    K = np.zeros(shape = (N+1,N+1))
+    U = np.zeros(shape = (N+1, M+1))
+    F = np.zeros(N+1)
+    i = np.arange(1,N)
+    for k in range(M-1, -1, -1):
+        a_ik = deltat*0.25*(-(i*r + 1/(T*delta_x)) - (sigma**2)*(i**2))
+        b_ik = deltat*0.25*(-(i*r + 1/(T*delta_x)) + (sigma**2)*(i**2))
+        c_ik = 1 + deltat*0.5*(sigma**2)*(i**2)
+        M[i, i-1] = b_ik
+        M[i, i] = c_ik
+        M[i,i+1] = a_ik
+        F[0] = 1
+        F[i] = U[i, k+1]
+        F[N] = 1
+        U[:,k] = linalg.solve(M,F)
+        return U
+        
+def Crank_Nicolson3(r, T, N, M, x_max, x_min, sigma): 
+    x = np.linspace(x_min, x_max, N + 1)
+    t = np.linspace(0, T, M + 1)
+    deltat = T / M
+    delta_x = (x_max - x_min) / N
+    A = np.zeros(N+1)
+    B = np.zeros(N+1)
+    C = np.zeros(N+1)
+
+    U = np.zeros(shape=(N+1,M+1))
+    for i in range(0, N+1):
+        U[i,N] = np.maximum(x[i],0)
+        
+    for k in range(0, M+1):
+        U[0, k] = 1 / (r * T) * (1 - np.exp(-r * (T-t[k]))) - x_min*np.exp(-r*(T-t[k]))
+        U[N, k] = 0
+      
+    for i in range(1, N + 1): 
+        A[i] = - deltat*0.25*(-(x[i]*r + 1/(T*delta_x)) - (sigma**2)*(x[i]**2))
+        B[i] = deltat*0.25*(-(x[i]*r + 1/(T*delta_x)) + (sigma**2)*(x[i]**2))
+        C[i] = 1 + deltat*0.5*(sigma**2)*(x[i]**2)
+    
+    i = np.arange(1,N)
+    for k in range(0, M):
+        U[i, k+1] = B[i]*U[i-1,k] + C[i]*U[i,k] + A[i]*U[i+1,k]
+        
+    plt.plot(U[:,0], x)  
+    plt.show()
+    return U
+
+U = Crank_Nicolson3(0.02, 1, 99, 999, 2, 0, 0.3)       
+        
+        
+
 if __name__ == '__main__':
 
     K = 50
@@ -340,13 +722,13 @@ if __name__ == '__main__':
 
     S0 = 1
     K = 10
-    r = 0.1
+    r = 0.04
     sigma = 0.5
     T = 1
-    N = 1000
-    Nmc = 10000
-    S = np.linspace(S0, 20, 20)
-    h = 0.000001
+    N = 100
+    Nmc = 1000
+    S = np.linspace(S0, 20, 40)
+    h = 1
 
     calls = np.zeros(len(S))
     callsFloat = np.zeros(len(S))
@@ -358,11 +740,11 @@ if __name__ == '__main__':
     gamm = np.zeros(len(S))
     edp = np.zeros(len(S))
 
-    # for i in range(len(S)):
+    for i in range(len(S)):
     #     # calls[i], calls_geo[i] = asian_option_price_call(S[i], r, K, T, N, sigma, Nmc)
     #     # puts[i], puts_geo[i] = asian_option_price_put(S[i], r, K, T, N, sigma, Nmc)
     #     # delt[i] = delta(S[i], r, K, T, sigma, N, Nmc, h)
-    #     gamm[i] = gamma(S[i], r, K, T, sigma, N, Nmc, h)
+           gamm[i] = gamma(S[i], r, K, T, sigma, N, Nmc, h)
     #     # print("Asian call option price:", calls[i])
     #     # print("Asian put option price:", puts[i])
     #     # edp[i] = EDP_2Dim_fixe(S0, r, K, T, N, sigma, Nmc)
@@ -389,12 +771,12 @@ if __name__ == '__main__':
     # plt.savefig('Graph\delta.png')
     # plt.show()
 
-    # plt.plot(S, gamm, label='gamma call')
-    # plt.xlabel('$S_0$')
-    # plt.ylabel('Price Value')
-    # plt.legend()
-    # plt.savefig('Graph\gamma.png')
-    # plt.show()
+    plt.plot(S, gamm, label='gamma call')
+    plt.xlabel('$S_0$')
+    plt.ylabel('Gamma')
+    plt.legend()
+    #plt.savefig('Graph\gamma.png')
+    plt.show()
     #
     # S0 = 10
     # K = 1
@@ -440,4 +822,47 @@ if __name__ == '__main__':
     S0 = 100
 
     V = Crank_Nicolson(r, T, N, M, x_max, x_min, sigma)
+    
+def Payoff_Asiatique (St, A, t, N, sigma, r, T):
+    delta_t = (T-t)/N
+    somme = 0
+    S = np.zeros(N+1)
+    S[0] = St
+    for n in range(0,N):
+        S[n+1] = S[n]*np.exp(r - sigma**2)*delta_t + sigma*np.random.randn(1)*np.sqrt(delta_t)
+        somme = somme + S[n]*delta_t
+    result = np.maximum((A*t + somme)/T,0)
+    return result
+
+def Prix_Asiatique(St, A, t, Nmc, N, sigma, r, T):
+    somme = 0
+    for k in range(1,Nmc+1):
+        somme = somme + Payoff_Asiatique(St, A, t, N, sigma, r, T)
+    result = np.exp(-r*(T-t))*somme/Nmc
+    return result
+
+def Prix_Asiatique_Final(t, Nmc, N, sigma, r, T):
+    St = np.zeros(201)
+    A = np.zeros(201)
+    prix_option = np.zeros(shape=(201, 201))
+    for k in range(1, 201):
+        for j in range(1,201):
+            St[k] = k - 1
+            A[j] = j -1 
+            prix_option[k, j] = Prix_Asiatique(St[k], A[j], t, Nmc, N, sigma, r, T)
+    plt.plot(St, A, prix_option)
+    plt.show()
+    
+T=1
+r=0.05
+sigma = 0.2
+Nmc = 200
+N = 100
+t = T/2
+
+Prix_Asiatique_Final(t, Nmc, N, sigma, r, T)
+        
+        
+        
+    
 
--- simpson_approxiamation.py
+++ simpson_approxiamation.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#------------------------------------------------------------------------------
+#------------------------------- Cas d'un Call --------------------------------
+#------------------------------------------------------------------------------
+
 def geometric_brownian_motion(t, S0, mu, sigma, W):
     return S0 * np.exp((mu - 0.5 * sigma ** 2) * t + sigma * W)
 
@@ -17,10 +21,7 @@ def simpson_rule(f, a, b, n):
 def asian_option_price_call(S0, r, K, T, N, sigma, Nmc):
     dt = T / N
     S_avg = np.zeros(Nmc)
-    S_avg_geo = np.zeros(Nmc)
-    S = np.zeros(N + 1)
     payoffs = np.zeros(Nmc)
-    payoffs_geo = np.zeros(Nmc)
 
     for i in range(Nmc):
         W = np.random.randn(1) * np.sqrt(dt)
@@ -54,7 +55,7 @@ if __name__ == '__main__':
     plt.title("Prix de l'option asiatique en fonction de la volatilité")
     plt.legend()
     plt.grid(True)
-    plt.savefig("Graph/simpsonHomer.png")
+    #plt.savefig("Graph/simpsonHomer.png")
     plt.show()
 
     S_values = np.linspace(50, 150, 10)  # Variations du prix de l'actif sous-jacent
@@ -77,5 +78,213 @@ if __name__ == '__main__':
     ax.set_ylabel('Prix de l\'actif sous-jacent (S)')
     ax.set_zlabel('Prix de l\'option')
     ax.set_title('Prix de l\'option asiatique en fonction de S et de la volatilité')
-    plt.savefig("Graph/simpsonMarge.png")
+    #plt.savefig("Graph/simpsonMarge.png")
     plt.show()
+    
+#------------------------------------------------------------------------------
+#------------------------------- Cas d'un Put ---------------------------------
+#------------------------------------------------------------------------------
+
+    
+def asian_option_price_put(S0, r, K, T, N, sigma, Nmc):
+    dt = T / N
+    S_avg = np.zeros(Nmc)
+    payoffs = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        W = np.random.randn(1) * np.sqrt(dt)
+        integral_approximation = simpson_rule(lambda t: geometric_brownian_motion(t, S0, r, sigma, W), 0, T,
+                                                  N)
+        S_avg[i] = integral_approximation
+        payoffs[i] = np.maximum(K - S_avg[i], 0)
+
+    option_price = np.exp(-r * T) * np.mean(payoffs)
+
+    return option_price
+
+if __name__ == '__main__':
+    # Paramètres
+    S0 = 100
+    r = 0.02
+    K = 100
+    T = 1
+    N = 100
+    sigmas = np.linspace(0.1, 0.5, 5)  # Variations de la volatilité
+    Nmc = 1000
+
+    # Calcul des prix des options asiatiques pour chaque valeur de volatilité
+    option_prices = [asian_option_price_put(S0, r, K, T, N, sigma, Nmc) for sigma in sigmas]
+
+    # Création du graphique
+    plt.figure(figsize=(8, 6))
+    plt.plot(sigmas, option_prices, label="Prix de l'option asiatique")
+    plt.xlabel("Volatilité")
+    plt.ylabel("Prix de l'option")
+    plt.title("Prix de l'option asiatique en fonction de la volatilité")
+    plt.legend()
+    plt.grid(True)
+    #plt.savefig("Graph/simpsonHomer.png")
+    plt.show()
+
+    S_values = np.linspace(50, 150, 10)  # Variations du prix de l'actif sous-jacent
+    sigmas = np.linspace(0.1, 0.5, 10)  # Variations de la volatilité
+    Nmc = 1000  # Nombre d'itérations de Monte Carlo constant
+
+    # Calcul des prix des options asiatiques pour chaque combinaison de S et sigma
+    option_prices = np.zeros((len(S_values), len(sigmas)))
+
+    for i, S in enumerate(S_values):
+        for j, sigma in enumerate(sigmas):
+            option_prices[i, j] = asian_option_price_put(S, r, K, T, N, sigma, Nmc)
+
+    # Création du graphique 3D
+    sigma_grid, S_grid = np.meshgrid(sigmas, S_values)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(sigma_grid, S_grid, option_prices, cmap='viridis')
+    ax.view_init(elev=20, azim=150)
+    ax.set_xlabel('Volatilité')
+    ax.set_ylabel('Prix de l\'actif sous-jacent (S)')
+    ax.set_zlabel('Prix de l\'option')
+    ax.set_title('Prix de l\'option asiatique en fonction de S et de la volatilité')
+    #plt.savefig("Graph/simpsonMarge.png")
+    plt.show()
+    
+#------------------------------------------------------------------------------
+#------------------------- Cas d'un Call flottant -----------------------------
+#------------------------------------------------------------------------------
+
+def asian_option_price_call_flottant(S0, r, K, T, N, sigma, Nmc):
+    dt = T / N
+    S_avg = np.zeros(Nmc)
+    payoffs = np.zeros(Nmc)
+    integral_approximation = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        W = np.random.randn(1) * np.sqrt(dt)
+        integral_approximation[i] = simpson_rule(lambda t: geometric_brownian_motion(t, S0, r, sigma, W), 0, T,
+                                              N)
+        S_avg[i] = np.mean(integral_approximation)
+        payoffs[i] = np.maximum(integral_approximation[Nmc - 1] - S_avg[i], 0)
+
+    option_price = np.exp(-r * T) * np.mean(payoffs)
+
+    return option_price
+
+if __name__ == '__main__':
+    # Paramètres
+    S0 = 100
+    r = 0.2
+    K = 100
+    T = 1
+    N = 100
+    sigmas = np.linspace(0.1, 0.5, 5)  # Variations de la volatilité
+    Nmc = 1000
+
+    # Calcul des prix des options asiatiques pour chaque valeur de volatilité
+    option_prices = [asian_option_price_call_flottant(S0, r, K, T, N, sigma, Nmc) for sigma in sigmas]
+
+    # Création du graphique
+    plt.figure(figsize=(8, 6))
+    plt.plot(sigmas, option_prices, label="Prix de l'option asiatique")
+    plt.xlabel("Volatilité")
+    plt.ylabel("Prix de l'option")
+    plt.title("Prix de l'option asiatique en fonction de la volatilité")
+    plt.legend()
+    plt.grid(True)
+    #plt.savefig("Graph/simpsonHomer.png")
+    plt.show()
+
+    S_values = np.linspace(50, 150, 10)  # Variations du prix de l'actif sous-jacent
+    sigmas = np.linspace(0.1, 0.5, 10)  # Variations de la volatilité
+    Nmc = 1000  # Nombre d'itérations de Monte Carlo constant
+
+    # Calcul des prix des options asiatiques pour chaque combinaison de S et sigma
+    option_prices = np.zeros((len(S_values), len(sigmas)))
+
+    for i, S in enumerate(S_values):
+        for j, sigma in enumerate(sigmas):
+            option_prices[i, j] = asian_option_price_call_flottant(S, r, K, T, N, sigma, Nmc)
+
+    # Création du graphique 3D
+    sigma_grid, S_grid = np.meshgrid(sigmas, S_values)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(sigma_grid, S_grid, option_prices, cmap='viridis')
+    ax.set_xlabel('Volatilité')
+    ax.set_ylabel('Prix de l\'actif sous-jacent (S)')
+    ax.set_zlabel('Prix de l\'option')
+    ax.set_title('Prix de l\'option asiatique en fonction de S et de la volatilité')
+    #plt.savefig("Graph/simpsonMarge.png")
+    plt.show()
+
+
+#------------------------------------------------------------------------------
+#------------------------- Cas d'un Put flottant ------------------------------
+#------------------------------------------------------------------------------
+    
+def asian_option_price_put_flottant(S0, r, K, T, N, sigma, Nmc):
+    dt = T / N
+    S_avg = np.zeros(Nmc)
+    payoffs = np.zeros(Nmc)
+    integral_approximation = np.zeros(Nmc)
+
+    for i in range(Nmc):
+        W = np.random.randn(1) * np.sqrt(dt)
+        integral_approximation[i] = simpson_rule(lambda t: geometric_brownian_motion(t, S0, r, sigma, W), 0, T,
+                                              N)
+        S_avg[i] = np.mean(integral_approximation)
+        payoffs[i] = np.maximum(S_avg[i] - integral_approximation[Nmc - 1], 0)
+
+    option_price = np.exp(-r * T) * np.mean(payoffs)
+
+    return option_price
+
+if __name__ == '__main__':
+    # Paramètres
+    S0 = 100
+    r = 0.2
+    K = 100
+    T = 1
+    N = 100
+    sigmas = np.linspace(0.1, 0.5, 5)  # Variations de la volatilité
+    Nmc = 1000
+
+    # Calcul des prix des options asiatiques pour chaque valeur de volatilité
+    option_prices = [asian_option_price_put_flottant(S0, r, K, T, N, sigma, Nmc) for sigma in sigmas]
+
+    # Création du graphique
+    plt.figure(figsize=(8, 6))
+    plt.plot(sigmas, option_prices, label="Prix de l'option asiatique")
+    plt.xlabel("Volatilité")
+    plt.ylabel("Prix de l'option")
+    plt.title("Prix de l'option asiatique en fonction de la volatilité")
+    plt.legend()
+    plt.grid(True)
+    #plt.savefig("Graph/simpsonHomer.png")
+    plt.show()
+
+    S_values = np.linspace(50, 150, 10)  # Variations du prix de l'actif sous-jacent
+    sigmas = np.linspace(0.1, 0.5, 10)  # Variations de la volatilité
+    Nmc = 1000  # Nombre d'itérations de Monte Carlo constant
+
+    # Calcul des prix des options asiatiques pour chaque combinaison de S et sigma
+    option_prices = np.zeros((len(S_values), len(sigmas)))
+
+    for i, S in enumerate(S_values):
+        for j, sigma in enumerate(sigmas):
+            option_prices[i, j] = asian_option_price_put_flottant(S, r, K, T, N, sigma, Nmc)
+
+    # Création du graphique 3D
+    sigma_grid, S_grid = np.meshgrid(sigmas, S_values)
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(sigma_grid, S_grid, option_prices, cmap='viridis')
+    ax.set_xlabel('Volatilité')
+    ax.set_ylabel('Prix de l\'actif sous-jacent (S)')
+    ax.set_zlabel('Prix de l\'option')
+    ax.set_title('Prix de l\'option asiatique en fonction de S et de la volatilité')
+    #plt.savefig("Graph/simpsonMarge.png")
+    plt.show()
+
+    
